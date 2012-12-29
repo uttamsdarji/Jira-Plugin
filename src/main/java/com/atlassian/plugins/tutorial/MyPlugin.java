8a01071e3481c3c86743f7e3831199be31ace44c
Mary Anthony <manthony@atlassian.com>
2012-12-28 17:58:16 -0800

    Moving source files from SVN to BB

---
+++ src/main/java/com/atlassian/plugins/tutorial/MyPlugin.java
@@ -0,0 +1,5 @@
+package com.atlassian.plugins.tutorial;
+
+public class MyPlugin
+{
+}
---
+++ src/test/java/com/atlassian/plugins/tutorial/MyPluginTest.java
@@ -0,0 +1,11 @@
+package com.atlassian.plugins.tutorial;
+
+import org.junit.Test;
+
+public class MyPluginTest
+{
+    @Test
+    public void testSomething()
+    {
+    }
+}
